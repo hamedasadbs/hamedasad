979dcd63969d3ad287149d359042ad714f2fe9bb
hamedasadbs <hamedasadollahi1998@gmail.com>
2024-05-30 11:25:46 -0700

    3/10

--- pages/homePage/index.js
+++ pages/homePage/index.js
@@ -1,6 +1,6 @@
 /*inner component*/
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { toast } from "react-hot-toast";
 /*style*/
 import styles from "./index.module.scss";
@@ -13,12 +13,61 @@ import { Context } from "../../middleware/library/context";
 
 export default function Home() {
   const theme = useContext(Context).theme[0];
+
+  useEffect(() => {
+    let radius = 250;
+    let icons = document.getElementsByClassName(styles.icon);
+    let angle = 0;
+    let step = (2 * Math.PI) / icons.length;
+    if (icons.length > 0) {
+      for (let i = 0; i < icons.length; i++) {
+        var x = Math.round(250 + radius * Math.cos(angle) - 20);
+        var y = Math.round(250 + radius * Math.sin(angle) - 20);
+        icons[i].style.left = x + "px";
+        icons[i].style.top = y + "px";
+        angle += step;
+      }
+    }
+  }, []);
+
   return (
     <article className={styles.homePage}>
       <section>
-        <h1 className={theme == "light" ? "text-sky-900" : "text-sky-500"}>
-          حامد اسداللهی
-        </h1>
+        <span>
+          <h1 className={theme == "light" ? "text-sky-900" : "text-sky-500"}>
+            حامد اسداللهی
+          </h1>
+          <div className={styles.contacts}>
+            <a target="-blank" href="https://www.instagram.com/hamedasadbs/">
+              <InstagramIcon
+                className={
+                  styles.contactIcons +
+                  " " +
+                  (theme == "light" ? "text-sky-500" : "text-slate-50")
+                }
+              />
+            </a>
+            <a target="-blank" href="https://linkedin.com/hamedasadbs/">
+              <LinkedInIcon
+                className={
+                  styles.contactIcons +
+                  " " +
+                  (theme == "light" ? "text-sky-500" : "text-slate-50")
+                }
+              />
+            </a>
+            <a target="-blank" href="https://github.com/hamedasadbs/">
+              <GitHubIcon
+                className={
+                  styles.contactIcons +
+                  " " +
+                  (theme == "light" ? "text-sky-500" : "text-slate-50")
+                }
+              />
+            </a>
+          </div>
+        </span>
+
         <h4 className={theme == "light" ? "text-slate-900" : "text-slate-50"}>
           برنامه نویس فرانت اند تحت فریمورک های React.js و Next.js و
           React-Native
@@ -43,50 +92,79 @@ export default function Home() {
           className={styles.myImage}
           src="/hamed.png"
           alt="Hamed Asadollahi"
-          width={632}
-          height={680}
+          width={700}
+          height={700}
           priority
         />
-        <div className={styles.contacts}>
-          <a target="-blank" href="https://www.instagram.com/hamedasadbs/">
-            <InstagramIcon
-              className={
-                styles.contactIcons +
-                " " +
-                (theme == "light" ? "text-sky-500" : "text-slate-50")
-              }
-            />
-          </a>
-          <a target="-blank" href="https://linkedin.com/hamedasadbs/">
-            <LinkedInIcon
-              className={
-                styles.contactIcons +
-                " " +
-                (theme == "light" ? "text-sky-500" : "text-slate-50")
-              }
-            />
-          </a>
-          <a target="-blank" href="https://github.com/hamedasadbs/">
-            <GitHubIcon
-              className={
-                styles.contactIcons +
-                " " +
-                (theme == "light" ? "text-sky-500" : "text-slate-50")
-              }
-            />
-          </a>
-        </div>
-        <svg
-          className={styles.backDecor}
-          viewBox="0 0 200 200"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="#be123c"
-            d="M42.8,-63.4C51.2,-61.1,50.8,-42.1,56.6,-26.7C62.3,-11.3,74.1,0.5,78.2,15.2C82.3,29.9,78.7,47.6,67.1,54.6C55.4,61.6,35.7,57.9,18.9,61.5C2.2,65,-11.6,75.6,-19,70.8C-26.5,65.9,-27.7,45.4,-33.2,32.5C-38.8,19.5,-48.7,14,-53.2,5.5C-57.8,-3,-57.1,-14.3,-52.9,-24.5C-48.8,-34.6,-41.2,-43.5,-31.8,-45.3C-22.4,-47.1,-11.2,-41.8,3,-46.5C17.2,-51.1,34.3,-65.6,42.8,-63.4Z"
-            transform="translate(100 100)"
-          />
-        </svg>
+      </div>
+
+      <div className={styles.icons}>
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale" : "") +
+            " " +
+            styles.js +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/javascript.svg")}
+          alt="javascript"
+        />
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale invert brightness-70" : "") +
+            " " +
+            styles.css +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/css.svg")}
+          alt="css"
+        />
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale invert brightness-70" : "") +
+            " " +
+            styles.html +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/html.svg")}
+          alt="html"
+        />
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale brightness-200" : "") +
+            " " +
+            styles.tailwind +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/tailwind.svg")}
+          alt="tailwind"
+        />
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale brightness-200" : "") +
+            " " +
+            styles.react +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/react.svg")}
+          alt="react"
+        />
+        <Image
+          className={
+            (theme == "dark" ? "filter grayscale invert brightness-70" : "") +
+            " " +
+            styles.nodejs +
+            " " +
+            styles.icon
+          }
+          src={require("../../public/icon/nodejs.svg")}
+          alt="nodejs"
+        />
       </div>
     </article>
   );
